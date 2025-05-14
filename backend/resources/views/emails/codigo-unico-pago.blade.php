dfd413f93fcd6d8034ce0198ec2dbc0db02e0e63
JAFETLP <jafetcana@gmail.com>
2025-05-13 22:33:35 -0300

    13/05/2025 22:33

--- backend/resources/views/emails/codigo-unico-pago.blade.php
+++ backend/resources/views/emails/codigo-unico-pago.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tu Código Único de Pago</title>
+    <title>Tu Código Único de Pre-inscripcion</title>
 </head>
 <body>
     <p>Estimado(a) {{ $nombresEncargado }},</p>
-    <p>Gracias por inscribir a su estudiante. Para continuar con el proceso, le proporcionamos el siguiente código único de pago:</p>
+    <p>Gracias por inscribir a su estudiante. Para continuar con el proceso, le proporcionamos el siguiente código único:</p>
     <h2>{{ $codigoUnico }}</h2>
-    <p>Utilice este código para realizar el pago correspondiente a la inscripción.</p>
+    <p>Utilice este código para generar su boleta de pago correspondiente a la inscripción.</p>
     <p>Por favor, siga las instrucciones de pago proporcionadas por la institución.</p>
     <p>Atentamente,</p>
     <p>Oh! Sansi</p>
